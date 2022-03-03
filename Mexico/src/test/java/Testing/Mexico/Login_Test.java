ddf99d551f14285a008fcce3e165ad6d66a87911
Yadav <soham.deepak-yadav@capgemini.com>
2022-03-03 18:04:06 +0530

    Second Commit

--- Mexico/src/test/java/Testing/Mexico/Login_Test.java
+++ Mexico/src/test/java/Testing/Mexico/Login_Test.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features ="C:\\Users\\SDEEPAKY\\Downloads\\Mexico\\src\\main\\java\\features\\login.feature" 
+@CucumberOptions(features ="C:\\Users\\SDEEPAKY\\git\\demo\\Mexico\\src\\main\\java\\features\\login.feature" 
 , glue ="step_def" ,monochrome = true , plugin = {"pretty","html:target/cucumber-pretty","junit:target/report.xml","json:target/jsonreport.json"})
 public class Login_Test {
 	
